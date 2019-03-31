43046d16daca0c000dbb358d592cdc31733f967e
Deepak Muley <deepak.muley@gmail.com>
2019-03-31 00:51:35 -0700

    added tree tests and output

---
+++ data-structures/tree/tree_test.go
@@ -0,0 +1,37 @@
+package tree
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/deepak-muley/golangexamples/data-structures/tree"
+)
+
+func Test_tree_traversals(t *testing.T) {
+	// (3 + 4) * 6
+	//      *
+	//  +      6
+	//3   4
+	root := new(tree.TreeNode)
+	root.SetValue("*")
+	root.AddRightChild("6")
+	plusNode := root.AddLeftChild("+")
+	plusNode.AddLeftChild("3")
+	plusNode.AddRightChild("4")
+
+	fmt.Println("Printing Tree")
+	root.PrintNode()
+	plusNode.PrintNode()
+
+	fmt.Println("Printing Inorder")
+	tree.PrintInorder(root)
+	fmt.Println()
+
+	fmt.Println("Printing Preorder")
+	tree.PrintPreorder(root)
+	fmt.Println()
+
+	fmt.Println("Printing Postorder")
+	tree.PrintPostorder(root)
+	fmt.Println()
+}
